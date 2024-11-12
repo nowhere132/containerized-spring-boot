f973c1ae122777124cb1ed88733c786d3fc6086c
nowhere132 <canh.manh123@gmail.com>
2024-11-12 17:50:47 +0700

    [nowhere132] Initial commit

---
+++ src/main/java/nowhere132/ContainerizedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package nowhere132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizedSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerizedSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nowhere132/ContainerizedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package nowhere132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerizedSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
